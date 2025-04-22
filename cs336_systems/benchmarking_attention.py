0f3b2d71c33dc5db84c89c437e8dc305f71b3098
Radi Cho <radicho123@gmail.com>
2025-04-22 07:31:39 +0300

    Simplify attention benchmarking.

--- cs336_systems/benchmarking_attention.py
+++ cs336_systems/benchmarking_attention.py
@@ -1,6 +1,6 @@
 import torch
-import pandas as pd
 import timeit
+import pandas as pd
 
 from cs336_basics.model import scaled_dot_product_attention
 
@@ -16,9 +16,9 @@ results = []
 
 for d_model in dmodels:
     for seq_len in seq_lens:
-        try:
-            print(d_model, seq_len)
+        print(d_model, seq_len)
 
+        try:
             Q = torch.randn(batch_size, seq_len, d_model, device=device, requires_grad=True)
             K = torch.randn(batch_size, seq_len, d_model, device=device, requires_grad=True)
             V = torch.randn(batch_size, seq_len, d_model, device=device, requires_grad=True)
@@ -31,26 +31,24 @@ for d_model in dmodels:
                 torch.cuda.synchronize()
                 Q.grad = K.grad = V.grad = None
 
-            def forward_pass():
-                out = scaled_dot_product_attention(Q, K, V, mask)
-                torch.cuda.synchronize()
-
             torch.cuda.reset_peak_memory_stats()
             start_mem = torch.cuda.memory_allocated(device)
-
-            forward_timer = timeit.Timer(forward_pass)
-            forward_time = forward_timer.timeit(number=n_runs) / n_runs
+            start = timeit.default_timer()
+            for _ in range(n_runs):
+                out = scaled_dot_product_attention(Q, K, V, mask)
+                torch.cuda.synchronize()
+            end = timeit.default_timer()
+            forward_time = (end - start) / n_runs
 
             mem_before_bwd = torch.cuda.memory_allocated(device)
 
-            def backward_pass():
-                out = scaled_dot_product_attention(Q, K, V, mask)
+            start = timeit.default_timer()
+            for _ in range(n_runs):
                 out.mean().backward()
                 torch.cuda.synchronize()
                 Q.grad = K.grad = V.grad = None
-
-            backward_timer = timeit.Timer(backward_pass)
-            backward_time = backward_timer.timeit(number=n_runs) / n_runs
+            end = timeit.default_timer()
+            backward_time = (end - start) / n_runs
 
             peak_mem = torch.cuda.max_memory_allocated(device)
         except:
@@ -65,6 +63,6 @@ for d_model in dmodels:
             "peak_mem_bytes": peak_mem
         })
 
-
 df = pd.DataFrame(results)
+df.to_csv("attention.csv")
 print(df)
