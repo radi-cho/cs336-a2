f6d4dbd55a4971b080ab3fb89e32140840cd5c88
Radi Cho <radicho123@gmail.com>
2025-04-30 03:57:40 +0300

    Also record with pytorch.

--- cs336_systems/benchmarking_commands.sh
+++ cs336_systems/benchmarking_commands.sh
@@ -9,6 +9,11 @@ uv run benchmarking_model.py --d_model 1600 --d_ff 6400 --num_layers 48 --num_he
 uv run benchmarking_model.py --d_model 2560 --d_ff 10240 --num_layers 32 --num_heads 32 --warmup_steps 5 --timing_steps 10
 uv run benchmarking_model.py --d_model 2560 --d_ff 10240 --num_layers 32 --num_heads 32 --warmup_steps 5 --timing_steps 10 --backward
 
+# Memory profiling
+uv run benchmarking_model.py --d_model 2560 --d_ff 10240 --num_layers 32 --num_heads 32 --warmup_steps 5 --timing_steps 10 --backward --adam_step --context_length 128
+uv run benchmarking_model.py --d_model 2560 --d_ff 10240 --num_layers 32 --num_heads 32 --warmup_steps 5 --timing_steps 10 --backward --adam_step --context_length 256
+uv run benchmarking_model.py --d_model 2560 --d_ff 10240 --num_layers 32 --num_heads 32 --warmup_steps 5 --timing_steps 10 --backward --adam_step --context_length 512
+
 uv run nsys profile -o result --force-overwrite true python benchmarking_model.py --d_model 768 --d_ff 3072 --num_layers 12 --num_heads 12 --warmup_steps 5 --timing_steps 10 --context_length 128
 
 uv run nsys profile -o c128_small --force-overwrite true python benchmarking_model.py --d_model 768 --d_ff 3072 --num_layers 12 --num_heads 12 --warmup_steps 5 --timing_steps 10 --context_length 128
--- cs336_systems/benchmarking_model.py
+++ cs336_systems/benchmarking_model.py
@@ -43,7 +43,7 @@ def benchmark_model(
     dtype_context = torch.autocast(device_type=device, dtype=torch.bfloat16) if mixed_precision else nullcontext()
     dataset = np.random.randint(low=0, high=vocab_size, size=(100000,))
 
-    # torch.cuda.memory._record_memory_history(max_entries=1000000)
+    torch.cuda.memory._record_memory_history(max_entries=1000000)
 
     for _ in range(warmup_steps):
         inputs, targets = get_batch(dataset, batch_size, context_length, device)
@@ -99,8 +99,8 @@ def benchmark_model(
 
         times.append(end - start)
 
-    # torch.cuda.memory._dump_snapshot("memory_snapshot.pickle")
-    # torch.cuda.memory._record_memory_history(enabled=None)
+    torch.cuda.memory._dump_snapshot("memory_snapshot.pickle")
+    torch.cuda.memory._record_memory_history(enabled=None)
 
     avg_time = np.mean(times)
     std_time = np.std(times)
